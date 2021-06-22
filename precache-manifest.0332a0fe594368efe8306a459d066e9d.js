e31d9f7f96b4e790a93bf00c718e6bd39e2e7ca3
shauandsss <shauands@gmail.com>
2021-06-22 06:01:34 -0300

    Updates

---
+++ precache-manifest.0332a0fe594368efe8306a459d066e9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0662f095c89eb545d1a4",
+    "url": "/KruskalJs/static/css/main.08e43385.chunk.css"
+  },
+  {
+    "revision": "0662f095c89eb545d1a4",
+    "url": "/KruskalJs/static/js/main.148d2236.chunk.js"
+  },
+  {
+    "revision": "ec75161ba34b30997dc3",
+    "url": "/KruskalJs/static/js/runtime~main.76873393.js"
+  },
+  {
+    "revision": "7f7450d717686a8774bc",
+    "url": "/KruskalJs/static/js/2.e9c57fd1.chunk.js"
+  },
+  {
+    "revision": "a91d217cad4ab08d31c2a2d8af9f377c",
+    "url": "/KruskalJs/index.html"
+  }
+];
